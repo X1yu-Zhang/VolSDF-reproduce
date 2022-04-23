b7000c22809607f6963a98366474b1e03a11b3d8
Amb1t10n <724985625@qq.com>
2022-04-23 19:22:11 +0800

    update sampling algorithm

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ get_gradient = lambda x, y:torch.autograd.grad(
 
 cal_density = lambda d, beta:1/beta*(0.5+0.5*torch.sign(d)*(1-torch.exp(-torch.abs(d)/beta)))
 
-laplace = lambda s, beta: torch.exp(-torch.aba(s)/beta)
+laplace = lambda s, beta: torch.exp(-torch.abs(s)/beta)
 
 get_sample_pts = lambda rays_o, rays_d, t: rays_o[:, None, :] + t[None, ...] * rays_d[:, None, :]
 
